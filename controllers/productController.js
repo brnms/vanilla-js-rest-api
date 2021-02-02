b73cf494e7518aef4bdd7086ac2f2af710b7b9bf
Bruno Santos <bruno.matasantos@gmail.com>
2021-02-01 21:45:27 -0300

    Add files via upload

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/productModel');
 
+// @desc Gets all products
+// #route GET /api/products
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll();
@@ -10,6 +12,47 @@ async function getProducts(req, res) {
     }
 }
 
+// @desc Gets single product
+// #route GET /api/product/:id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+
+        if(!product){
+            res.writeHead(404, { 'Content-type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Product not Found'}));
+        }else{
+            res.writeHead(200, { 'Content-type': 'application/json' });
+            res.end(JSON.stringify(product));
+        }
+       
+    } catch(error){
+        console.log(error);
+    }
+}
+
+
+// @desc Create a product
+// #route POST /api/products
+async function createProduct(req, res) {
+    try {
+       const product = {
+           title: 'Test Product',
+           description: 'This is my product',
+           price: 100
+       }
+
+       const newProduct = await Product.create(product);
+       res.writeHead(201, { 'Content-Type': 'application/json'})
+       return res.end(JSON.stringify(newProduct));
+    } catch(error){
+        console.log(error);
+    }
+}
+
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct,
+    createProduct
 }
--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,6 @@
 const products = require('../data/products.json');
+const { v4: uuidv4 } = require('uuid');
+const { writeDataToFile } = require('../utils');
 
 function findAll() {
     return new Promise((resolve, reject) => {
@@ -6,6 +8,24 @@ function findAll() {
     });
 }
 
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        const product = products.find((p) => p.id === id)
+        resolve(product);
+    });
+}
+
+function create(product) {
+    return new Promise((resolve, reject) => {
+        const newProduct = {id: uuidv4(), ...product};
+        products.push(newProduct);
+        writeDataToFile('./data/products.json', products);
+        resolve(newProduct); 
+    });
+}
+
 module.exports = {
-    findAll
+    findAll,
+    findById,
+    create
 }
